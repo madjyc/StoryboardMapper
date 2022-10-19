d86ef1fb40c1a39b5e567f6eeb37fb65e02f50ae
Jean-Yves Chasle <jeanyves.chasle@free.fr>
2022-10-19 21:42:14 +0200

    Deleted project files to keep only screencopies

--- Scripts/GraphData.gd
+++
@@ -1,6 +0,0 @@
-extends Resource
-class_name GraphData
-
-export var version: float
-export var nodes: Array
-export var connections: Array
--- Scripts/GraphNodeData.gd
+++
@@ -1,7 +0,0 @@
-extends Resource
-class_name GraphNodeData
-
-export var name: String
-export var offset: Vector2
-export var rect_size: Vector2
-export var extra_data = {}
